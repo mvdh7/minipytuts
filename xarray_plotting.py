b0a6830c637ee80f87ab688f853abb388943d7fa
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-11-06 11:55:42 +0100

    Update xarray_plotting.py

--- xarray_plotting.py
+++ xarray_plotting.py
@@ -12,20 +12,19 @@ gebco.elevation.plot()
 #%% Select a single grid row and plot it
 gebco.elevation.sel(lon=-8, method='nearest').plot()
 
-
 #%% Import satellite data directly from online
-oc = xr.open_dataset("https://oceandata.sci.gsfc.nasa.gov:443/opendap/MODISA/L3SMI/2020/0101/AQUA_MODIS.20200101.L3m.DAY.SST.sst.9km.nc")
+oc = xr.open_dataset("https://oceandata.sci.gsfc.nasa.gov:443/opendap/MODISA/L3SMI/2020/0601/AQUA_MODIS.20200601_20200630.L3m.MO.NSST.sst.9km.nc")
 
 #%% Coarsen the data then plot
 oc.sst.coarsen(lat=5, lon=5, boundary="trim").mean().plot(vmin=0, vmax=30, cmap="magma")
 
 #%% Select a subset of the data
 # Get latitudes in northern hemisphere
-northern_hem = oc.lat[oc.lat > 0]
-lon_range = oc.lon[(oc.lon > -100) & (oc.lon < 0)]
+lat_range = oc.lat[(oc.lat > 40) & (oc.lat < 50)]
+lon_range = oc.lon[(oc.lon > 25) & (oc.lon < 42)]
 
 # Plot it
-oc.sst.sel(lat=northern_hem, lon=lon_range, method="nearest").plot(vmin=0, vmax=30, cmap="magma")
+oc.sst.sel(lat=lat_range, lon=lon_range, method="nearest").plot(vmin=0, vmax=30, cmap="magma")
 
 #%% Interpolate the gaps in an extremely basic way
 (
